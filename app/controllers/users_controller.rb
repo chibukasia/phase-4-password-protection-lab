dbaae12a044c937cbedf0b1cb9212ecea01abe6b
Chibukasia Nelson <chibukasianelson@gmail.com>
2022-10-05 09:52:27 +0300

    commit tree d5350a6d9bce74c8bf26a1fc3f6de389a44fb77f

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :user_not_found
+    def create
+        user = User.create(user_params)
+        if user.valid?
+          session[:user_id] = user.id
+          render json: user, status: :created
+        else
+          render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+    end
+
+
+    def show 
+        user = User.find(session[:user_id])
+        render json: user, status: :ok 
+        # if user 
+            
+        # else
+        #     render json: {errors: "Invalid username or password"}, status: :unauthorized
+        # end
+    end
+    # Private methods 
+    private 
+
+    def user_params 
+        params.permit(:username, :password, :password_confirmation)
+    end 
+
+    def user_not_found 
+        render json: {errors: "Invalid username or password"}, status: :unauthorized  
+    end
+    
+end
